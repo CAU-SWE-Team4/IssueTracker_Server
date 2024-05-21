0c2ef82f5a2651ad20505566c11eb35af0497c39
yeojin <yeojin7010+L@gmail.com>
2024-05-21 11:34:03 +0900

    Spring Initialize

---
+++ src/main/java/com/example/issuetracker_server/IssueTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.issuetracker_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssueTrackerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssueTrackerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/issuetracker_server/IssueTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.issuetracker_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueTrackerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
