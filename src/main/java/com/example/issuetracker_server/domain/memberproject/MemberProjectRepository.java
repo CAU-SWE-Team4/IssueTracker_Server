883e610ece410f1c0c3e30e3e58d2a9cedfc6b90
michang <minseok128128@gmail.com>
2024-05-26 22:03:55 +0900

    Added *Repository : 각 domain에 맞는 jpa repository class 구현

---
+++ src/main/java/com/example/issuetracker_server/domain/memberproject/MemberProjectRepository.java
@@ -0,0 +1,6 @@
+package com.example.issuetracker_server.domain.memberproject;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberProjectRepository extends JpaRepository<MemberProject, Long> {
+}
