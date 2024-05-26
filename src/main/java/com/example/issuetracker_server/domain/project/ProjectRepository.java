dc0e6fc372276014aad8d5b0210436c32200fa8e
michang <minseok128128@gmail.com>
2024-05-26 21:56:40 +0900

    Update ProjectRepository : Query 형태는 최대한 사용하지 않는 것이 좋을 것으로 판단

--- src/main/java/com/example/issuetracker_server/domain/project/ProjectRepository.java
+++ src/main/java/com/example/issuetracker_server/domain/project/ProjectRepository.java
@@ -1,12 +1,6 @@
 package com.example.issuetracker_server.domain.project;
 
-import com.example.issuetracker_server.domain.project.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
-public interface ProjectRepository extends JpaRepository<Project, Long>{
-    @Query("SELECT p FROM Project p")
-    List<Project> findAll();
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
