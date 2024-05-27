4bc7c41236a31ee1e31583289a91ba769452c3bc
minseok128 <minseok128128@gmail.com>
2024-05-27 12:44:22 +0900

    Update MemberController : login과 signUp에 해당하는 Controller 정의 및 적절한 service 연결

--- src/main/java/com/example/issuetracker_server/controller/MemberController.java
+++ src/main/java/com/example/issuetracker_server/controller/MemberController.java
@@ -1,14 +1,40 @@
 package com.example.issuetracker_server.controller;
 
+import com.example.issuetracker_server.dto.member.MemberLoginRequest;
+import com.example.issuetracker_server.dto.member.MemberSignUpRequest;
 import com.example.issuetracker_server.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/user")
 public class MemberController {
 
     private final MemberService memberService;
 
+    @PostMapping("/login")
+    public ResponseEntity<Void> login(@RequestBody MemberLoginRequest request) {
+        boolean isAuthenticated = memberService.login(request);
+        if (isAuthenticated) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
 
+    @PostMapping("/signUp")
+    public ResponseEntity<Void> signUp(@RequestBody MemberSignUpRequest request) {
+        try {
+            memberService.signUp(request);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+    }
 }
