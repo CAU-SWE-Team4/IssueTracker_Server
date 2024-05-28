286c5319416f825ceb4c1dbaa70aae4d89919081
minseok128 <minseok128128@gmail.com>
2024-05-28 20:17:55 +0900

    Update MemberProjectService : getRole service 구현

--- src/main/java/com/example/issuetracker_server/service/memberproject/MemberProjectService.java
+++ src/main/java/com/example/issuetracker_server/service/memberproject/MemberProjectService.java
@@ -1,8 +1,13 @@
 package com.example.issuetracker_server.service.memberproject;
 
+import com.example.issuetracker_server.domain.memberproject.Role;
 import com.example.issuetracker_server.dto.project.ProjectsSaveRequestDto;
+
+import java.util.Optional;
 
 public interface MemberProjectService {
 
     Long save(Long projectId, ProjectsSaveRequestDto.Member member);
+
+    Optional<Role> getRole(String uId, Long pId);
 }
