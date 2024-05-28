ad4d57a806e051e7e476c7f64c783877427e82e7
minseok128 <minseok128128@gmail.com>
2024-05-28 22:33:44 +0900

    Update IssueRepository : 6가지 정렬방식 구현

--- src/main/java/com/example/issuetracker_server/domain/issue/IssueRepository.java
+++ src/main/java/com/example/issuetracker_server/domain/issue/IssueRepository.java
@@ -2,5 +2,18 @@ package com.example.issuetracker_server.domain.issue;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    List<Issue> findByProjectIdOrderByTitleAsc(Long projectId);
+
+    List<Issue> findByProjectIdOrderByTitleDesc(Long projectId);
+
+    List<Issue> findByProjectIdOrderByCreatedDateAsc(Long projectId);
+
+    List<Issue> findByProjectIdOrderByCreatedDateDesc(Long projectId);
+
+    List<Issue> findByProjectIdOrderByStateAsc(Long projectId);
+
+    List<Issue> findByProjectIdOrderByStateDesc(Long projectId);
 }
