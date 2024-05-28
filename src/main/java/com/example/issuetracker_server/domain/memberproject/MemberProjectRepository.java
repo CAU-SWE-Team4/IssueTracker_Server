b3a99a64da7553e2870355254007ee099e8cd413
minseok128 <minseok128128@gmail.com>
2024-05-28 20:17:33 +0900

    Update MemberProjectRepository : findByMemberIdAndProjectId 함수 추가

--- src/main/java/com/example/issuetracker_server/domain/memberproject/MemberProjectRepository.java
+++ src/main/java/com/example/issuetracker_server/domain/memberproject/MemberProjectRepository.java
@@ -2,5 +2,8 @@ package com.example.issuetracker_server.domain.memberproject;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberProjectRepository extends JpaRepository<MemberProject, Long> {
+    Optional<MemberProject> findByMemberIdAndProjectId(String memberId, Long projectId);
 }
