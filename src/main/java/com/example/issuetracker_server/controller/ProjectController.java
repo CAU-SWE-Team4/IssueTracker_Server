9cec5c3e80193f9d3879ea2e1e097cb2430ba436
yeojin <yeojin7010+L@gmail.com>
2024-06-01 17:11:43 +0900

    Update project: 필요 없는 코드 삭제

--- src/main/java/com/example/issuetracker_server/controller/ProjectController.java
+++ src/main/java/com/example/issuetracker_server/controller/ProjectController.java
@@ -77,19 +77,12 @@ public class ProjectController {
         Optional<Project> optionalProject = projectService.findById(projectId);
         if (optionalProject.isPresent()) {
             Project project = optionalProject.get();
-//            if (requestDto.getTitle() != null) {
-//                projectService.update(projectId, requestDto);
-//                return ResponseEntity.status(HttpStatus.OK).build();
-//            }
             if (requestDto.getMembers() != null) {
                 projectService.update(projectId, requestDto);
                 List<MemberProject> memberProjects = memberprojectService.getMemberProjectByProjectId(project.getId());
-                System.out.println("memberprojects: "+ memberProjects);
                 memberprojectService.deleteAll(memberProjects);
-                System.out.println("\nDeleteAll\n");
                 for (ProjectRequestDto.Member member : requestDto.getMembers()) {
                     memberprojectService.save(projectId, member);
-                    System.out.println("Save MemberProject: " + member);
                 }
                 return ResponseEntity.status(HttpStatus.OK).build();
 
